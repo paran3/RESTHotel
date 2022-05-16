41dcea34734af4249f8fc2db6cc6762f44076038
paran3 <lemibi11@naver.com>
2022-05-16 19:26:26 +0900

    main class name changed.(to 'Resthotel')

--- src/main/java/com/restcompany/Resthotel.java
+++ src/main/java/com/restcompany/Resthotel.java
@@ -2,22 +2,18 @@ package com.restcompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableScheduling
 @EnableAsync
-public class RestCompanyApplication {
+public class Resthotel {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestCompanyApplication.class, args);
+		SpringApplication.run(Resthotel.class, args);
 	}
 
 //	@Bean
